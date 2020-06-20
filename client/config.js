e2b55def211192c59c9f61fa59bb35f5cb57d6eb
Nigel Reoch <nreoch9@gmail.com>
2020-06-19 20:10:24 -0400

    fixed client config

--- client/config.js
+++ client/config.js
@@ -6,9 +6,9 @@ const config = {
     wsEndpoint: "ws://localhost:8000/graphql",
   },
   production: {
-    clientEndpoint: "https://auth2.developal/graphql",
-    serverEndpoint: "https://auth2.developal/graphql",
-    wsEndpoint: "wss://auth.developal/graphql",
+    clientEndpoint: "https://auth2.developal.ca/graphql",
+    serverEndpoint: "https://auth2.developal.ca/graphql",
+    wsEndpoint: "wss://auth2.developal.ca/graphql",
   },
 }[env];
 
