6a9ae134203a37cf919f0bfb24b4eb8caaadf934
Nigel Reoch <nreoch9@gmail.com>
2020-06-22 20:47:21 -0400

    Added jumbotron

--- client/components/Index.js
+++ client/components/Index.js
@@ -1,10 +1,18 @@
 import { Component } from "react";
-
+import { Jumbotron } from "reactstrap";
 class Index extends Component {
   render() {
     return (
       <div>
-        <h1>Index Page</h1>
+        <Jumbotron className="p-2 pl-3 pr-3 mt-3">
+          <h2>
+            Authentication system boilerplate application using NextJS, GraphQL, Apollo Client,
+            Apollo Server, NodeJS, MongoDB, and Nginx
+          </h2>
+          <a href="https://github.com/nreoch25/nextjs-graphql-hooks-auth" target="_blank">
+            Click here to checkout the Github repo
+          </a>
+        </Jumbotron>
       </div>
     );
   }
